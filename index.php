174c85c77513c86079604fb35eabdd1b8cd757b4
mambo0815 <mambo0815@gmx.de>
2016-04-17 17:57:22 +0200

    Erstellung Startseite

--- index.php
+++ index.php
@@ -4,14 +4,14 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- Die 3 Meta-Tags oben *müssen* zuerst im head stehen; jeglicher sonstiger head-Inhalt muss *nach* diesen Tags kommen -->
-    <title>Bootstrap-Basis-Vorlage</title>
+    <!-- Die 3 Meta-Tags oben *m&uuml;ssen* zuerst im head stehen; jeglicher sonstiger head-Inhalt muss *nach* diesen Tags kommen -->
+    <title>K&uuml;chen Willemsen | Nottuln - Immer eine gute Wahl!</title>
 
     <!-- Bootstrap -->
     <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Unterstützung für Media Queries und HTML5-Elemente in IE8 über HTML5 shim und Respond.js -->
-    <!-- ACHTUNG: Respond.js funktioniert nicht, wenn du die Seite über file:// aufrufst -->
+	<link href="css/style.css" rel="stylesheet">
+    <!-- Unterst&uuml;tzung f&uuml;r Media Queries und HTML5-Elemente in IE8 &uuml;ber HTML5 shim und Respond.js -->
+    <!-- ACHTUNG: Respond.js funktioniert nicht, wenn du die Seite &uuml;ber file:// aufrufst -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
@@ -21,84 +21,155 @@
    
     <div class="container">
       <div class="header clearfix">
-		 <!-- Static navbar -->
-		  <nav class="navbar navbar-inverse">
-			<div class="container-fluid">
-			  <div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-				  <span class="sr-only">Toggle navigation</span>
-				  <span class="icon-bar"></span>
-				  <span class="icon-bar"></span>
-				  <span class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand" href="#">Project name</a>
-			  </div>
-			  <div id="navbar" class="navbar-collapse collapse">
-				<ul class="nav navbar-nav">
-				  <li class="active"><a href="#">Home</a></li>
-				  <li><a href="#">About</a></li>
-				  <li><a href="#">Contact</a></li>
-				  <li class="dropdown">
-					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-					<ul class="dropdown-menu">
-					  <li><a href="#">Action</a></li>
-					  <li><a href="#">Another action</a></li>
-					  <li><a href="#">Something else here</a></li>
-					  <li role="separator" class="divider"></li>
-					  <li class="dropdown-header">Nav header</li>
-					  <li><a href="#">Separated link</a></li>
-					  <li><a href="#">One more separated link</a></li>
-					</ul>
-				  </li>
-				</ul>
-				<ul class="nav navbar-nav navbar-right">
-				  <li class="active"><a href="./">Default <span class="sr-only">(current)</span></a></li>
-				  <li><a href="../navbar-static-top/">Static top</a></li>
-				  <li><a href="../navbar-fixed-top/">Fixed top</a></li>
-				</ul>
-			  </div><!--/.nav-collapse -->
-			</div><!--/.container-fluid -->
-		  </nav>
-        <h3 class="text-muted">Project name</h3>
+		<img src="gfx/logo.gif" />
+        <a class="navbar-brand" href="#"></a>
       </div>
+	</div>  
 
-      <div class="jumbotron">
-        <h1>Jumbotron heading</h1>
-        <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-        <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
-      </div>
-
-      <div class="row marketing">
-        <div class="col-lg-6">
-          <h4>Subheading</h4>
-          <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
-
-          <h4>Subheading</h4>
-          <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
-
-          <h4>Subheading</h4>
-          <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+	   <!-- Main jumbotron -->
+    <div class="jumbotron jumbotron-head">
+      <div class="container">
+		<img src="gfx/topbild_1.jpg" class="img-responsive"></img>
+      
+	  <span class="teaser-homepage">vorsprung durch erfahrung<br />
+	  seit &uuml;ber 25 jahren</span>
+	
+		<!-- Static navbar -->
+      <nav class="navbar navbar-inverse">
+          
+		  <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button> 
+          </div>
+		  
+          <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav nav-justified">
+              <li class="active"><a href="#">&Uuml;ber uns</a></li>
+              <li><a href="#">Leistungen</a></li>
+              <li><a href="#">Studio</a></li>
+			  <li><a href="#">Referenzen</a></li>
+			  <li><a href="#">Aktuelles</a></li>
+			  <li><a href="#">Angebote</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        
+      </nav> <!--/.navbar navbar-default --> 
+	  
+	  </div><!--/.container -->
+    </div><!--/.jumbotron jumbotron-head -->
+	  
+	<div class="container">
+      
+	  <!-- row of columns -->
+      <div class="row">
+        <div class="col-md-4">
+          <h2>Leistungen</h2>
+          <p>Wir bieten Ihnen eine optimale Beratung mit fotorealistischer 3D Pr&auml;sentation Ihrer Traumk&uuml;che. Neugierig?</p>
+          <p><a class="btn btn-primary" href="#" role="button">Mehr erfahren</a></p>
         </div>
-
-        <div class="col-lg-6">
-          <h4>Subheading</h4>
-          <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
-
-          <h4>Subheading</h4>
-          <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
-
-          <h4>Subheading</h4>
-          <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+        <div class="col-md-4">
+          <h2>K&uuml;chenstudio</h2>
+          <p>... finden Sie alles was das Herz begehrt. Schauen Sie doch einmal vorbei und lassen sich inspirieren.</p>
+          <p><a class="btn btn-primary" href="#" role="button">Mehr erfahren</a></p>
+		</div>
+        <div class="col-md-4">
+          <h2>Referenzen</h2>
+          <p>Hier m&ouml;chten wir Ihnen eine kleine Auswahl unserer fertiggestellten Arbeiten pr&auml;sentieren.</p>
+          <p><a class="btn btn-primary" href="#" role="button">Mehr erfahren</a></p>
         </div>
-      </div>
+      </div><!--/.row -->
 
-      <footer class="footer">
-        <p>&copy; 2015 Company, Inc.</p>
-      </footer>
+	  <!--/two-columns content-area -->
+	  <div class="double-line"><h3>&Uuml;ber uns</h3></div>
+      <h4>K&uuml;chen Willemsen - <strong>Vorsprung durch Erfahrung</strong>.</h4>
+      <div class="row">
+			<div class="col-md-8">Wir leben K&uuml;che in der 2. Generation! Im Jahr 1986 gr&uuml;ndete Erich Willemsen die K&uuml;chen Willemsen GmbH in Rosendahl.Sohn Stefan Willemsen trat 1990 in den v&auml;terlichen Betrieb ein.<br />1997 zog die Firma nach Nottuln und hier pr&auml;sentieren wir bis heute auf &uuml;ber 400 qm stets wechselnde Qualit&auml;tsk&uuml;chen mit Markentechnik f&uuml;r jeden Geschmack und jedes Budget.<br />L&auml;ngst sind wir weit &uuml;ber die Tore des M&uuml;nsterlandes bekannt als kompetenter Partner f&uuml;r Einbauk&uuml;chen. Wir setzen Ihre W&uuml;nsche fachkundig um und erarbeiten mit Ihnen Ihre Traumk&uuml;che. Qualit&auml;t, Funktion und Design stehen bei uns im Fokus.<br />Abgerundet durch fachkundige Beratung und Planung sowie perfekten Service in der gesamten Abwicklung zum bestm&ouml;glichen Preis.<br />
+			</div>
+			<div class="col-md-4">
+				<img src="gfx/kuechenstudio.jpg" class="img-responsive" />
+			</div>
+		</div><!-- end  row -->
+		<div class="row clearfix">
+			<div class="col-xs-4 col-md-2">
+				<img src="gfx/stefan_willemsen-s.jpg" class="img-responsive" />
+			</div>
+			<div class="col-xs-8 col-md-10">
+				<p>Getreu dem Motto: Ihre Empfehlung ist unsere sch&ouml;nste Referenz!</p>
+				<p>Sie suchen Ihre Traumk&uuml;che? - Besuchen Sie uns.</p>
+				<p>Wir freuen uns auf Sie!</p>
+				<p><strong>Herzlichst<br />Stefan Willemsen &amp; Team</strong></p>
+				</p>
+			</div>
+		</div>
+		<div class="double-line"><img src="gfx/doppellinie.png" /></div>
+      </div><!-- /.container-->
+	  
+      
+	  <footer class="footer">
+		<div class="container">
+			<!-- row of columns -->
+			<div class="row">
+				
+				<div class="col-md-4">
+				  
+				  <div class="row">
+				    <div class="col-md-12"><h4>Sitemap</h4></div>
+						<div class="col-xs-6">
+							<ul class="">
+								<li><a href="#">&Uuml;ber uns</a></li>
+								<li><a href="#" class="">Leistungen</a></li>
+								<li><a href="#" class="">Studio</a></li>
+								<li><a href="#" class="">Referenzen</a></li>
+								<li><a href="#" class="">Aktuelles</a></li>
+							</ul>
+						</div>		  
+						<div class="col-xs-6">
+								<ul class="">
+								<li><a href="#">Angebote</a></li>
+								<li><a href="#" class="">Der Weg zu uns</a></li>
+								<li><a href="#" class="">Kontakt</a></li>
+								<li><a href="#" class="">Partnerfirmen</a></li>
+								<li><a href="#" class="">Impressum</a></li>
+							</ul>
+						</div>	
+				  </div>
+				</div><!-- /.col-md-3 -->	
+				
+				
+				<div class="col-md-6 pics">
+				  <h4>Aktuelle K&uuml;chen</h4>
+					<div class="row">
+					<div class="col-xs-3"><img class="img-responsive" src="gfx/kuechen_kl_1.jpg" /></div>
+					<div class="col-xs-3"><img class="img-responsive" src="gfx/kuechen_kl_2.jpg" /></div>
+					<div class="col-xs-3"><img class="img-responsive" src="gfx/kuechen_kl_3.jpg" /></div>
+					</div>
+				</div>
+				<div class="col-md-2">
+				  <h4>Service Kontakt</h4>
+				  <p>
+					  K&uuml;chen Willemsen GmbH<br />
+					  Appelh&uuml;lsener Str. 22<br />
+					  48301 Nottuln
+				  </p>
+				  <p>
+					  Tel 0 25 02 - 17 30<br />
+					  Fax 0 25 02 - 17 87
+				  </p>
+				  <p>
+					  info@kuechen-willemsen.de
+				  </p>
+				</div><!--/col-md-2 -->
+			</div><!--/.row -->
+		</div>
+      </footer><!-- /.footer -->
 
-    </div> <!-- /container -->
+   
 
-    <!-- jQuery (wird für Bootstrap JavaScript-Plugins benötigt) -->
+    <!-- jQuery (wird f&uuml;r Bootstrap JavaScript-Plugins ben&ouml;tigt) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <!-- Binde alle kompilierten Plugins zusammen ein (wie hier unten) oder such dir einzelne Dateien nach Bedarf aus -->
     <script src="lib/bootstrap/js/bootstrap.min.js"></script>
